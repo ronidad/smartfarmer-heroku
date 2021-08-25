7e0ef5fe0ceff2758cb1d0f14b14a2386586a488
Ronald Mosomi <ronaldmosomi@gmail.com>
2021-08-25 07:02:37 +0300

    Add heroku to publish

--- streamlit-app.py
+++ streamlit-app.py
@@ -9,15 +9,21 @@ st.subheader(
 )
 
 Average_Prices = pd.read_csv("df_crops.csv").set_index("Produce_Variety")
-# regions_to_sell= pd.read_csv("df_regisons.csv").set_index("CROPS")
 months = pd.read_csv("df_crops_per_month.csv").set_index("Month")
+product_counties = pd.read_csv("df_to_products_per_county.csv").set_index("COMMODITY")
+regions_to_plant = pd.read_csv("df_regisons.csv").set_index("CROPS")
 
 
 # st.bar_chart(df)
 
 selected_status = st.sidebar.selectbox(
     "Select Data to view",
-    options=["Average Prices", "Products per county", "products per Months"],
+    options=[
+        "Average Prices",
+        "Products per county",
+        "products per Months",
+        "Regions to Grow",
+    ],
 )
 
 if selected_status == "Average Prices":
@@ -25,7 +31,19 @@ if selected_status == "Average Prices":
     st.subheader("Products grouped into various categories")
     st.bar_chart(Average_Prices)
 
+if selected_status == "Products per county":
+    st.title("Products per county")
+    st.subheader("These products do better in the counties shown")
+    st.table(product_counties)
+
 if selected_status == "products per Months":
     st.title("number of products per month")
     st.subheader("May has the ighest product hence the best month for many")
     st.bar_chart(months)
+
+if selected_status == "Products per county":
+    st.title("Regions where specific crops do yield the highest")
+    st.subheader(
+        "The following crops do better in these counties as per the yields per Hactare"
+    )
+    st.table(regions_to_plant)
